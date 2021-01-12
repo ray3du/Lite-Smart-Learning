92e2956af0eb5c0387d11950e670802763967280
ray3du <edwinngugi149@gmail.com>
2021-01-12 22:11:36 +0300

    Initial commit

---
+++ app/src/main/java/com/alveen/m_soma/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.alveen.m_soma
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
+import com.google.firebase.auth.FirebaseAuth
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance();
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val btnRegistration = findViewById<Button>(R.id.registrationButton)
+        val btnLogin = findViewById<Button>(R.id.loginButton)
+        val emailUser = findViewById<EditText>(R.id.email)
+        val passwordUser = findViewById<EditText>(R.id.password)
+        val spinnerProgress = findViewById<ProgressBar>(R.id.spinnerLogin)
+
+        setSupportActionBar(toolbar)
+
+        //onclick listener
+        btnRegistration.setOnClickListener {
+            val intent = Intent(this@MainActivity, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+
+        //onclick listener
+        btnLogin.setOnClickListener {
+            spinnerProgress.isVisible = true
+            val email: String = emailUser.text.toString()
+            val password: String = passwordUser.text.toString()
+
+            if (email != "" || password != "") {
+                verifyUser(email, password, spinnerProgress)
+            } else {
+                spinnerProgress.isVisible = false
+                Toast.makeText(this, "Email and password cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun verifyUser(email: String, password: String, spinnerProgress: ProgressBar){
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(
+                this
+            ) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val intent = Intent(this, MainActivity2::class.java)
+                    spinnerProgress.isVisible = false
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    spinnerProgress.isVisible = false
+                    Toast.makeText(
+                        this, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                // ...
+            }
+    }
+}
---
+++ app/src/main/java/com/alveen/m_soma/RegistrationActivity.kt
@@ -0,0 +1,89 @@
+package com.alveen.m_soma
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
+class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+        //Firebase
+        mAuth = FirebaseAuth.getInstance()
+
+        val btnRegister = findViewById<Button>(R.id.buttonLayout)
+        val btnBack = findViewById<ImageView>(R.id.backButton)
+        val username = findViewById<EditText>(R.id.emailRegister)
+        val passCode = findViewById<EditText>(R.id.passwordRegister)
+        val confirm = findViewById<EditText>(R.id.passwordConfirm)
+        val spinnerProgress = findViewById<ProgressBar>(R.id.spinnerRegister)
+
+        //Returning back button
+        btnBack.setOnClickListener {
+            val intent = Intent(this@RegistrationActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        //Registration button
+        btnRegister.setOnClickListener {
+            //register fun
+
+            spinnerProgress.isVisible = true
+                if (confirm.text.toString() != passCode.text.toString()){
+                    spinnerProgress.isVisible = false
+                    Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+                    passCode.setText("")
+                    confirm.setText("")
+                }else {
+                    createUser(username, passCode, spinnerProgress)
+                }
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser: FirebaseUser? = mAuth.currentUser
+        println(currentUser)
+    }
+
+    private fun createUser(user: EditText, pass: EditText, spinnerProgress: ProgressBar){
+        val email: String = user.text.toString()
+        val password: String = pass.text.toString()
+
+        if (email != "" || password != ""){
+            mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                    this
+                ) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        spinnerProgress.isVisible = false
+                        val intent = Intent(this, MainActivity2::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        spinnerProgress.isVisible = false
+                        Toast.makeText(
+                            this, "Registration failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }else{
+            spinnerProgress.isVisible = false
+            Toast.makeText(
+                this, "Fields cannot be empty",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
